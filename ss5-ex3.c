f58c1e037c107ae61b97e5d8d994f41bf2e7e5cc
BMVu2403 <buiminhvu06hn@gmail.com>
2024-11-25 20:50:52 +0700

    Add files via upload

---
+++ ss5-ex3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Nhap vao mot so nguyen duong: ");
+    scanf("%d", &n);
+
+    if (n > 0) {
+    	int i; 
+        for (i = 1; i <= n; i++) {
+            sum += i;
+        }
+        printf("Tong cac so tu 1 den %d la: %d\n", n, sum);
+    } else {
+        printf("So nhap vao phai la so nguyen duong.\n");
+    }
+
+    return 0;
+}
+
